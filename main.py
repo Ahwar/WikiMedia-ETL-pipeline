c7e1000b06cdc5f0916a1e8c0aa9cf37d7018265
ahwar <20405797+Ahwar@users.noreply.github.com>
2024-01-16 07:21:03 +0000

    fix: improved http error handling

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 # Installed packages
 import requests
 from bs4 import BeautifulSoup
+
 # built-in imports
 import logging
 from datetime import datetime
+import traceback
+import sys
+
 # custom imports
 from utils.logger import get_logger
 from utils.api import make_http_get_request
@@ -11,26 +15,37 @@ from utils.api import make_http_get_request
 # configure logging
 get_logger()
 
+
 def get_directory_list(url, top_n=10):
     try:
         # Send an HTTP request to the webpage
-        response = make_http_get_request(url) 
-        soup = BeautifulSoup(response.text, 'html.parser')
+        response = make_http_get_request(url)
+        if response is None:
+            logging.error(
+                f"Error Occured while fetching the URL: {url}, exiting the program..."
+            )
+            sys.exit(1)
+        soup = BeautifulSoup(response.text, "html.parser")
 
         # Find all links in the HTML content
-        links = soup.find_all('a')
+        links = soup.find_all("a")
 
         # Extract top most directory links
-        dir_list = [link.get('href') for link in links if link.get('href').strip().endswith('/')]
+        dir_list = [
+            link.get("href") for link in links if link.get("href").strip().endswith("/")
+        ]
         return dir_list[-top_n:]
     except Exception as e:
-        logging.error(f"Error: {e}")
+        logging.error(f"Error: {traceback.print_exc()}")
         return None
-    
+
 
 def main():
-    dir_list = get_directory_list("https://dumps.wikimedia.org/other/clickstream/", top_n=1)
+    dir_list = get_directory_list(
+        "https://dumps.wikimedia.org/other/clickstream/", top_n=1
+    )
     print("top_n directories", dir_list)
+
 
 if __name__ == "__main__":
     # executes only if run as a script
--- utils/api.py
+++ utils/api.py
@@ -1,16 +1,28 @@
 import requests
+import traceback
 
 import logging
+
+
 def make_http_get_request(url):
     try:
         response = requests.get(url)
-        logging.info(f"Response code: {response.status_code}")
-        return response
+        if response.status_code == 200:
+            return response
+        elif response.status_code == 404:
+            logging.error(f"The URL '{url}' not found")
+            return None
+        elif response.status_code == 500:
+            logging.error(f"Internal server error at '{url}'")
+            return None
+        elif response.status_code == 400:
+            logging.error(f"Bad request at '{url}'")
+            return None
     except requests.exceptions.ConnectionError as e:
-        logging.error(f"Connection error: {e}")
+        logging.error(f"Connection error: {traceback.print_exc()}")
     except requests.exceptions.Timeout as e:
-        logging.error(f"Timeout error: {e}")
+        logging.error(f"Timeout error: {traceback.print_exc()}")
     except requests.exceptions.HTTPError as e:
-        logging.error(f"HTTP error: {e}")
+        logging.error(f"HTTP error: {traceback.print_exc()}")
     except requests.exceptions.RequestException as e:
-        logging.error(f"Request error: {e}")
+        logging.error(f"Request error: {traceback.print_exc()}")
